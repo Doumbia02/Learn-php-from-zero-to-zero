fd174ec89587b35b46fa917cb0f0bac23df8ad99
Doumbia02 <mansoureh225@gmail.com>
2024-07-12 16:53:02 +0600

    new

---
+++ 2_Php_comments.php
@@ -0,0 +1,8 @@
+<?php
+//Here I am displaying my name.
+#Anything after this symbol will not affect ...
+echo"My name is Youssoufe Mansoureh";
+/* 
+This is a block 
+of
+comments */
---
+++ 3_php_variables.php
@@ -0,0 +1,6 @@
+<?php
+
+$name="Youssouf Doumbia";
+$Nationality="Ivorian";
+$age=20;
+echo"My name is {$name}, I an {$Nationality}  and I am {$age} years old ";
